d4ccc01a3e809a3f55f2029f39f0652a954f7ab1
Gena <malkerovg99@gmail.com>
2022-06-19 15:29:43 +0300

    one task donw

---
+++ net/CodeWars/Tasks/Permutations/Impl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasks.Permutations
+{
+    public static class Impl
+    {
+        public static List<string> SinglePermutations(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return null;
+
+            if (s.Length == 1)
+                return new List<string>(1) { s };
+            var res = new List<string>() /*{s}*/;
+            Permute(s, "", res);
+            
+            return res.Distinct().ToList();
+        }
+
+        static void Permute(string s, string answer, List<string> answerList)
+        {
+            if (s.Length == 0)
+            {
+                answerList.Add(answer);
+                return;
+            }
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var ch = s[i];
+                var leftSubstr = s.Substring(0, i);
+                var rightSubstr = s.Substring(i + 1);
+                var rest = leftSubstr + rightSubstr;
+                Permute(rest, answer + ch, answerList);
+            }
+        }
+    }
+}
---
+++ net/CodeWars/Tasks/Permutations/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace Tasks.Permutations
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Example1()
+        {
+            Assert.AreEqual(new List<string> { "a" }, Impl.SinglePermutations("a").OrderBy(x => x).ToList());
+        }
+
+        [Test]
+        public void Example2()
+        {
+            Assert.AreEqual(new List<string> { "ab", "ba" }, Impl.SinglePermutations("ab").OrderBy(x => x).ToList());
+        }
+
+        [Test]
+        public void Example3()
+        {
+            Assert.AreEqual(new List<string> { "aabb", "abab", "abba", "baab", "baba", "bbaa" }, Impl.SinglePermutations("aabb").OrderBy(x => x).ToList());
+        }
+    }
+}
