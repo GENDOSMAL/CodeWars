5d9ae86ef182c6e40377f1b434dbb22e3fbcb43a
Gena <malkerovg99@gmail.com>
2022-06-19 15:00:16 +0300

    first task

---
+++ net/CodeWars/Tasks/Windows/Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Tasks.Windows
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase(1, "-----\n" +
+                     "|.|.|\n" +
+                     "|-+-|\n" +
+                     "|.|.|\n" +
+                     "-----")]
+        [TestCase(3, "---------\n" +
+                     "|...|...|\n" +
+                     "|...|...|\n" +
+                     "|...|...|\n" +
+                     "|---+---|\n" +
+                     "|...|...|\n" +
+                     "|...|...|\n" +
+                     "|...|...|\n" +
+                     "---------")]
+        public void FixedTests(int num, string expected)
+        {
+            Assert.AreEqual(expected, WinwdowTaskImpl.MakeAWindow(num));
+        }
+    }
+}
---
+++ net/CodeWars/Tasks/Windows/WinwdowTaskImpl.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Tasks.Windows
+{
+    public static class WinwdowTaskImpl
+    {
+        public static string MakeAWindow(int num)
+        {
+            var res = new StringBuilder();
+
+            var baseStr= new string('.', num); 
+            var floor = $"|{baseStr}|{baseStr}|";
+            var startAndEnd = new string('-', floor.Length);
+            res.Append(startAndEnd.AppendNewLine());
+            for (int i = 0; i < num; i++)
+            {
+                res.Append(floor.AppendNewLine());
+            }
+
+            res.Append($"|{new string('-', num)}+{new string('-', num)}|".AppendNewLine());
+            for (int i = 0; i < num; i++)
+            {
+                res.Append(floor.AppendNewLine());
+            }
+            res.Append(startAndEnd);
+
+            return res.ToString();
+        }
+
+        private static string AppendNewLine(this string lineToAppend)
+        {
+            return $"{lineToAppend}\n";
+        }
+    }
+}
